d9f7e4ebf5c8bcea9e5ed052aefc11f562bb41bf
Daniel da Rocha <danrocha@gmail.com>
2020-08-20 07:21:48 +0200

    add disqus

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: ['~/plugins/vue-disqus'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
