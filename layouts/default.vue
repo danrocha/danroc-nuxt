2331657d2d8aafbc500e275ada365e6299b9040f
Daniel da Rocha <danrocha@gmail.com>
2020-08-20 07:19:37 +0200

    move layout element to own components

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,35 +2,20 @@
   <div>
     <svg-sprite />
     <header class="mb-12">
-      <div class="flex flex-wrap items-center justify-between">
+      <div class="sm:flex sm:items-center sm:justify-between">
         <h1 class="font-mono text-5xl text-pink-600">
           danroc.dev
         </h1>
-        <ul class="flex space-x-2 text-lg text-gray-800">
-          <li>
-            <a href="//twitter.com/danrocc" target="_blank" rel="nofollow"
-              ><svg-icon icon="twitter"
-            /></a>
-          </li>
-          <li>
-            <a href="//github.com/danrocha" target="_blank" rel="nofollow"
-              ><svg-icon icon="github"
-            /></a>
-          </li>
-          <li>
-            <a href="//linkedin.com/in/danrocha" target="_blank" rel="nofollow"
-              ><svg-icon icon="linkedin"
-            /></a>
-          </li>
-        </ul>
+        <social-icons />
       </div>
     </header>
     <main class="main">
       <Nuxt />
     </main>
-    <footer class="pt-4 my-8 text-sm text-gray-500 border-t border-gray-500">
-      <span>Copyright © {{ new Date().getFullYear() }}. </span>
-      <span>Powered by <a href="//nuxtjs.org"> Nuxt.js </a></span>
-    </footer>
+    <base-footer />
   </div>
 </template>
+
+<script>
+export default {}
+</script>
