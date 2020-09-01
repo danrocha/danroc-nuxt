c357763776a6ef11e03b7e7b663f015143aee3fb
Daniel da Rocha <danrocha@gmail.com>
2020-09-01 11:43:58 +0200

    refactor home

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,61 @@
 <template>
   <div class="container">
     <div class="home">
-      <section
-        class="mb-8 prose content sm:ml-12 sm:mb-12 md:w-2/3 lg:m-0 lg:w-full"
-      >
-        <p>
-          I'm <strong>Daniel da Rocha</strong>, a Brazilian architect and web
-          developer.
+      <section class="mb-8 sm:ml-12 sm:mb-12 md:w-2/3 lg:m-0 lg:w-full">
+        <nuxt-content :document="homeAbout" class="text-xl" />
+        <p class="flex items-center justify-end">
+          <a href="#about" class="link">read more</a
+          ><svg-icon icon="arrow-down" />
         </p>
-        <p>
-          I apply skills learned in architecture to my development efforts: the
-          ability to deal with complex problems, to devise simple and elegant
-          solutions, and to deal with diverse and complex team configurations.
-        </p>
-        <p>
-          Things I am currently building include:
-          <a href="//awarded.to" target="_blank" class="font-semibold link"
-            >Awarded</a
-          >,
-          <a
-            href="//thearchitecturelist.com"
-            target="_blank"
-            class="font-semibold link"
-            >The Architecture List</a
-          >,
-          <a
-            href="//theforeignarchitect"
-            target="_blank"
-            class="font-semibold link"
-            >The Foreign Architect</a
-          >, and <strong>Berlin Architectural Network</strong> (WIP).
-        </p>
-        <dl class="flex mt-8">
-          <div class="mr-8">
-            <dt class="text-xs text-gray-500 uppercase trackign wide">
-              Homebase & Currently in
-            </dt>
-            <dd>🇩🇪Berlin</dd>
-          </div>
-        </dl>
+      </section>
+      <!-- List PROJECTS -->
+      <section class="section">
+        <a name="projects" />
+        <h2 class="blue">
+          Projects
+        </h2>
+        <div class="h-8" />
+        <div class="project-grid sm:ml-12 md:w-2/3 lg:w-full lg:ml-0 xl:w-2/3">
+          <project-list-item
+            v-for="project in projects"
+            :key="project.slug"
+            :project="project"
+          />
+        </div>
       </section>
       <section></section>
-      <section></section>
       <!-- List posts -->
-      <section class="posts">
-        <h2 class="mb-4 text-base tracking-wide text-gray-500 uppercase">
-          Latest articles
+      <section class="section">
+        <a name="posts" />
+        <h2 class="green">
+          Articles
         </h2>
+        <div class="h-8" />
         <div class="post-grid sm:ml-12 md:w-2/3 lg:w-full lg:ml-0 xl:w-2/3">
           <post-list-item v-for="post in posts" :key="post.slug" :post="post" />
         </div>
+      </section>
+
+      <!-- ABOUT -->
+      <section class="section">
+        <a name="about" />
+        <h2 class="yellow">
+          About
+        </h2>
+        <div class="h-8" />
+        <p class="mb-8 text-center">
+          <img
+            :src="myPhoto"
+            width="200"
+            height="200"
+            alt="Avatar from Daniel da Rocha"
+            class="inline-block"
+          />
+        </p>
+        <section class="mb-8 -ml-8 sm:ml-12 sm:mb-12 md:w-2/3 lg:m-0 lg:w-full">
+          <nuxt-content :document="about" class="prose" />
+        </section>
+        <social-icons class="justify-center" />
       </section>
     </div>
   </div>
@@ -59,18 +65,52 @@
 export default {
   async asyncData({ $content }) {
     const posts = await $content('posts').fetch()
+    const projects = await $content('projects').sortBy('order').fetch()
+    const homeAbout = await $content('homeAbout').fetch()
+    const about = await $content('about').fetch()
     return {
       posts,
+      about,
+      homeAbout,
+      projects,
     }
+  },
+  computed: {
+    myPhoto() {
+      return this.$cloudinary().url(`danrocdev/danieldarocha.jpg`, {
+        width: 200,
+        height: 200,
+        radius: 'max',
+        crop: 'thumb',
+        effect: 'grayscale',
+      })
+    },
   },
 }
 </script>
 
 <style>
-.posts h2::before {
+.section h2 {
+  @apply font-mono text-sm font-bold tracking-wide uppercase;
+}
+.section h2::before {
   display: block;
   content: '';
-  @apply w-24 mb-2 border-t-2 border-blue-400;
+  @apply mb-1 w-32 border-t-2 border-black;
+}
+.section h2.blue::before {
+  @apply border-blue-500;
+}
+.section h2.green::before {
+  @apply border-green-500;
+}
+.section h2.yellow::before {
+  @apply border-yellow-500;
+}
+
+.project-grid {
+  display: grid;
+  grid-gap: 2rem;
 }
 
 /* @screen md {
@@ -81,11 +121,14 @@ export default {
   }
 } */
 
+.home {
+  display: grid;
+  grid-gap: 3rem;
+}
+
 @screen lg {
   .home {
-    display: grid;
     grid-template-columns: 30% 1fr;
-    grid-gap: 2rem;
   }
 }
 </style>
