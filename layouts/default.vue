2117538daad437273a1bda4643d4720bf9eaf8c0
Daniel da Rocha <danrocha@gmail.com>
2020-09-07 09:31:45 +0200

    fix header layout alignment

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,21 +2,20 @@
   <div class="relative max-w-lg lg:ml-12 lg:mt-12 xl:ml-20 xl:mt-20">
     <svg-sprite />
     <div
-      class="absolute z-0 w-full h-24 mt-4 ml-8 text-teal-900 pattern-dots-sm"
+      class="absolute z-0 w-full h-24 mt-3 ml-8 text-teal-900 pattern-dots-sm"
     />
     <header class="relative z-10 mb-12">
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <template v-if="$route.path === '/'">
+      <template v-if="$route.path === '/'">
+        <logo />
+      </template>
+      <template v-else>
+        <nuxt-link to="/">
+          <span v-if="$route.path !== '/'" class="text-teal-500">&larr;</span>
           <logo />
-        </template>
-        <template v-else>
-          <nuxt-link to="/">
-            <span v-if="$route.path !== '/'" class="text-teal-500">&larr;</span>
-            <logo />
-          </nuxt-link>
-        </template>
-        <social-icons class="flex-none hidden sm:flex" />
-      </div>
+        </nuxt-link>
+      </template>
+      <div class="hidden sm:block">&nbsp;</div>
+      <social-icons class="hidden sm:flex" />
     </header>
     <main class="relative z-20 main">
       <Nuxt />
@@ -29,3 +28,16 @@
 <script>
 export default {}
 </script>
+
+<style scoped>
+header {
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
+
+@screen sm {
+  header {
+    grid-template-columns: auto 1fr auto;
+  }
+}
+</style>
