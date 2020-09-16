63d6691a5591b5dc53980aee201b423fb9cb8f34
Daniel da Rocha <danrocha@gmail.com>
2020-09-16 17:58:39 +0200

    reimplement plugin aos

--- plugins/aos.ts
+++ plugins/aos.ts
@@ -2,5 +2,5 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default ({ app }: any) => {
-  app.AOS = new (AOS.init as any)({ disable: 'phone', once: true }) // eslint-disable-line new-cap
+  app.AOS = new (AOS.init as any)({ disable: 'phone' }) // eslint-disable-line new-cap
 }
