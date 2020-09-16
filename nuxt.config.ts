43c2aa579913ecc58c84632fa05449d72a9848d0
Daniel da Rocha <danrocha@gmail.com>
2020-09-16 17:42:39 +0200

    add plugin vue-content-placeholders

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,11 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~/plugins/vue-disqus', { src: '~/plugins/aos', ssr: false }],
+  plugins: [
+    '~/plugins/vue-disqus',
+    '~/plugins/vue-content-placeholders',
+    { src: '~/plugins/aos', mode: 'client' },
+  ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
